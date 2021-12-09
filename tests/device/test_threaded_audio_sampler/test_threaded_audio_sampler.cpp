9bce87a346c3c373a91206f6e30874c0a27ea1bf
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-12-09 14:25:49 +0100

    Add outline test file for ThreadedAudioSampler

---
+++ tests/device/test_threaded_audio_sampler/test_threaded_audio_sampler.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file    test_threaded_audio_sampler.cpp
+ * @brief   Tests the ThreadedAudioSampler.
+ * @author  Kacper Kowalski - kacper.s.kowalski@gmail.com
+ */
+
+/**
+ * @defgroup ThreadedAudioSamplerTests     Tests of ThreadedAudioSampler
+ * @{
+ */
+void test_single_sample_is_obtained();
+void test_proper_number_of_samples_is_collected_within_specific_period();
+void test_sampling_frequency_is_stable();
+
+/** @} */ // End of group ThreadedAudioSamplerTests
