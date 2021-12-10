cd8e9336369c913af9fc385441cee0c6b702453a
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-12-10 13:00:36 +0100

    Add missing definitions

--- tests/device/test_threaded_audio_sampler/test_threaded_audio_sampler.cpp
+++ tests/device/test_threaded_audio_sampler/test_threaded_audio_sampler.cpp
@@ -11,5 +11,21 @@
 void test_single_sample_is_obtained();
 void test_proper_number_of_samples_is_collected_within_specific_period();
 void test_sampling_frequency_is_stable();
-
 /** @} */ // End of group ThreadedAudioSamplerTests
+
+// =====================================================================================================================
+// Tests' definition
+// =====================================================================================================================
+
+void test_single_sample_is_obtained()
+{
+}
+
+void test_proper_number_of_samples_is_collected_within_specific_period()
+{
+}
+
+void test_sampling_frequency_is_stable()
+{
+}
+
