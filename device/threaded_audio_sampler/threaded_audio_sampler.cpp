65d540945138c1c9fbaab2ad7ba58a1c7e30e230
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-12-13 20:39:28 +0100

    Initially implement ADC1 interrupt handler
    This is taken from:
    
        STM32Cube_FW_L4_V1.17.0/Projects/NUCLEO-L496ZG/Examples_LL/ADC/ADC_SingleConversion_TriggerSW_IT
    
    I changed the code from the interupt handler a little bit, to expect End
    Of Sequence flag to be set, since this is how I configured the SW via
    CubeMX.
    
    There is a global static function handler added, called
    adc1_interrupt_handler, that can be used by the singleton to run some
    custom code. The custom code is called from the interrupt if it is
    initialized.

--- device/threaded_audio_sampler/threaded_audio_sampler.cpp
+++ device/threaded_audio_sampler/threaded_audio_sampler.cpp
@@ -10,6 +10,10 @@
 
 #include "threaded_audio_sampler.hpp"
 
+#include "adc.h"
+
+static std::function<void(uint16_t raw_sample)> adc1_interrupt_handler;
+
 ThreadedAudioSampler::ThreadedAudioSampler()
 {
 }
@@ -43,6 +47,28 @@ void ThreadedAudioSampler::stop()
 
 extern "C" void ADC1_IRQHandler(void)
 {
+    auto is_conversion_finished{[]() {
+        // TODO: check if LL_ADC_IsActiveFlag_EOC() shall be used instead (end of conversion).
+        // Currently we check for End of Sequence.
+        return LL_ADC_IsActiveFlag_EOS(ADC1);
+    }};
+
+    auto clear_conversion_flag{[]() {
+        LL_ADC_ClearFlag_EOS(ADC1);
+    }};
+
+    if (is_conversion_finished())
+    {
+        clear_conversion_flag();
+        uint16_t raw_sample{LL_ADC_REG_ReadConversionData12(ADC1)};
+
+        if (adc1_interrupt_handler)
+        {
+            adc1_interrupt_handler(raw_sample);
+        }
+    }
+
+    // TODO: handle errors from LL_ADC_IsActiveFlag_OVR(ADC1) (overrun detection).
 }
 
 ThreadedAudioSampler::Error::Error(const char* message)
