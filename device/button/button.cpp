d1472f922f9518e0701480797edd88a5dad94123
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2021-12-20 17:43:26 +0100

    Fix button press detect when not released
    We didn't wait for the button to be released and because of that the
    tests have passed quickly after first button press. The first button
    press was detected as multiple. This is because we didn't wait for the
    button release.
    
    The second test is implemented and passes.

--- device/button/button.cpp
+++ device/button/button.cpp
@@ -11,16 +11,31 @@
 
 bool Button::wait_milliseconds_for_press(unsigned int timeout_milliseconds)
 {
-    unsigned polling_interval_ms{10};
-    bool is_press_detected{false};
-    for (unsigned time_ms{0}; time_ms < timeout_milliseconds; time_ms += polling_interval_ms)
-    {
-        if (auto is_pin_cleared{!LL_GPIO_IsInputPinSet(BUTTON0_GPIO_Port, BUTTON0_Pin)}; is_pin_cleared)
+    auto wait_for_falling_edge{[=]() {
+        unsigned polling_interval_ms{10};
+        bool is_press_detected{false};
+        for (unsigned time_ms{0}; time_ms < timeout_milliseconds; time_ms += polling_interval_ms)
         {
-            is_press_detected = true;
-            break;
+            if (auto is_pin_cleared{!LL_GPIO_IsInputPinSet(BUTTON0_GPIO_Port, BUTTON0_Pin)}; is_pin_cleared)
+            {
+                is_press_detected = true;
+                break;
+            }
+            os::wait_milliseconds(polling_interval_ms);
         }
-        os::wait_milliseconds(polling_interval_ms);
-    }
+        return is_press_detected;
+    }};
+
+    auto wait_for_rising_edge{[]() {
+        do
+        {
+            if (auto is_pin_set{LL_GPIO_IsInputPinSet(BUTTON0_GPIO_Port, BUTTON0_Pin)}; is_pin_set)
+                break;
+        } while (true);
+    }};
+
+    auto is_press_detected{wait_for_falling_edge()};
+    if (is_press_detected)
+        wait_for_rising_edge();
     return is_press_detected;
 }
--- tests/device/test_button/test_button_semi_automatic.cpp
+++ tests/device/test_button/test_button_semi_automatic.cpp
@@ -30,6 +30,7 @@ using namespace std::chrono_literals;
 void test_single_press_is_awaited()
 {
     SerialLogger logger;
+    logger.log(LogLevel::info) << "Testing for single, simple button pressing... ";
     logger.log(LogLevel::info) << "Press the button ...";
     Button button;
     auto is_button_pressed{button.wait_for_press(15s)};
@@ -38,7 +39,21 @@ void test_single_press_is_awaited()
 
 void test_multiple_presses_are_awaited()
 {
-    TEST_IGNORE();
+    SerialLogger logger;
+
+    logger.log(LogLevel::info) << "Testing for multiple, simple button pressing in a row... ";
+
+    logger.log(LogLevel::info) << "Press the button once...";
+
+    Button button;
+
+    auto is_button_pressed_first_time{button.wait_for_press(15s)};
+    TEST_ASSERT_TRUE(is_button_pressed_first_time);
+
+    logger.log(LogLevel::info) << "Press the button for the second time now...";
+
+    auto is_button_pressed_second_time{button.wait_for_press(15s)};
+    TEST_ASSERT_TRUE(is_button_pressed_second_time);
 }
 
 void test_multiple_presses_are_awaited_for_multiple_instances()
