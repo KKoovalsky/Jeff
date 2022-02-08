3a0800e25a2e8c9963cb1ce638ea74f41feca7d5
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-02-08 16:30:58 +0100

    Fix clock generation by enabling outputs
    Without the call to *EnableAllOutputs() no output was generated at the
    pin A5. This fixes 2MHz clock generation.

--- device/filter_cutoff_setter_clock/filter_cutoff_setter_clock.cpp
+++ device/filter_cutoff_setter_clock/filter_cutoff_setter_clock.cpp
@@ -24,10 +24,12 @@ void FilterCutoffSetterClock::start()
 {
     LL_TIM_CC_EnableChannel(TIM16, LL_TIM_CHANNEL_CH1);
     LL_TIM_EnableCounter(TIM16);
+    LL_TIM_EnableAllOutputs(TIM16);
 }
 
 void FilterCutoffSetterClock::stop()
 {
+    LL_TIM_DisableAllOutputs(TIM16);
     LL_TIM_CC_DisableChannel(TIM16, LL_TIM_CHANNEL_CH1);
     LL_TIM_DisableCounter(TIM16);
 }
