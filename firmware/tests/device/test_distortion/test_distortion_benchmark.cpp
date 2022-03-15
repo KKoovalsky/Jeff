c19e240c0f98228746f6775b0fe2b3ca2edac9b6
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2022-03-15 14:43:49 +0100

    Use non-default-initialized BatchOfSamples
    The benchmark didn't have a lot of value when the BatchOfSamples
    contained only zeros.
    The generated samples are scrapped from AudioDacWithDma, for input sine
    wave with frequency 1.34 kHz. This means that the
    generate_batch_of_samples() will return a snapshot of 1.34 kHz sine wave
    sampled with 44.1 kHz.

--- firmware/tests/device/test_distortion/test_distortion_benchmark.cpp
+++ firmware/tests/device/test_distortion/test_distortion_benchmark.cpp
@@ -29,10 +29,20 @@ void test_applying_distortion_is_not_too_long()
 
     Distortion distortion{0.6};
 
+    auto generate_batch_of_samples{[]() {
+        return BatchOfSamples{-0.333f, -0.253f, -0.165f, -0.079f, -0.004f, 0.092f,  0.179f,  0.262f,  0.328f,  0.387f,
+                              0.438f,  0.468f,  0.476f,  0.456f,  0.441f,  0.404f,  0.345f,  0.265f,  0.198f,  0.122f,
+                              0.028f,  -0.072f, -0.152f, -0.232f, -0.304f, -0.375f, -0.424f, -0.449f, -0.461f, -0.472f,
+                              -0.459f, -0.424f, -0.364f, -0.308f, -0.241f, -0.153f, -0.059f, 0.024f,  0.101f,  0.195f,
+                              0.281f,  0.341f,  0.390f,  0.435f,  0.474f,  0.475f,  0.456f,  0.428f,  0.388f,  0.334f,
+                              0.260f,  0.179f,  0.099f,  0.024f,  -0.085f, -0.174f, -0.247f, -0.313f, -0.379f, -0.437f,
+                              -0.459f, -0.462f, -0.465f, -0.455f};
+    }};
+
     for (auto& duration : test_durations)
     {
         auto begin{timer.time_microseconds()};
-        distortion.apply(BatchOfSamples{});
+        distortion.apply(generate_batch_of_samples());
         auto end{timer.time_microseconds()};
         duration = end - begin;
     }
